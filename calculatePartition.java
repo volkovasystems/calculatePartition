47adc0e9f22504862eccea2dacfd64598d4e9b47
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-04-15 20:45:38 +0800

    Updated calculatePartition procedure

--- calculatePartition.java
+++ calculatePartition.java
@@ -1,13 +1,39 @@
 package calculatePartition;
 
 import java.math.BigInteger;
+import java.math.BigDecimal;
 
 public class calculatePartition{
+	private static final int DEFAULT_ROOT_FACTOR = 2;
+	
 	public static void main( String... parameters ){
+		String spatialSize = parameters[ 0 ];
+		int rootFactor = DEFAULT_ROOT_FACTOR;
+		if( parameters.length == 2 ){
+			try{
+				rootFactor = Integer.parseInt( parameters[ 1 ] );
+			}catch( Exception exception ){
+				System.err.print( exception.getMessage( ) );
+				return;
+			}
+		}
 
+		BigDecimal partitionFactor = calculatePartition( spatialSize, rootFactor );
+		System.out.print( partitionFactor.toString( ) );
 	}
 
-	public static final BigInteger calculatePartition( ){
-		return null;		
+	public static final BigDecimal calculatePartition( String spatialSize, int rootFactor ){
+		BigDecimal size = new BigDecimal( spatialSize );
+		
+		BigDecimal root = BigDecimal.ONE;
+		BigDecimal previousRoot = BigDecimal.ZERO;
+		do{
+			previousRoot = root;
+			root = root.subtract( root.pow( rootFactor ).subtract( size )
+				.divide( ( new BigDecimal( rootFactor ) ).multiply( root.pow( rootFactor - 1 ) ) ) );
+			System.out.println( root.toString( ) );
+		}while( root.compareTo( previousRoot ) == 0 );
+
+		return root;		
 	}
 }
